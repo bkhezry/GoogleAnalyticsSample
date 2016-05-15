98feff5a298aff988d6d4e229fe2abc79d2f0d0f
bkhezry <bkhezry@gmail.com>
2016-05-15 19:59:41 +0430

    add Tracker to MainActivity

--- app/src/main/java/ir/bkhezry/googleanalyticssample/MainActivity.java
+++ app/src/main/java/ir/bkhezry/googleanalyticssample/MainActivity.java
@@ -3,11 +3,19 @@ package ir.bkhezry.googleanalyticssample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.analytics.Tracker;
+
+import ir.bkhezry.googleanalyticssample.Application.AnalyticsApplication;
+
 public class MainActivity extends AppCompatActivity {
+    private Tracker mTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Obtain the shared Tracker instance.
+        AnalyticsApplication application = (AnalyticsApplication) getApplication();
+        mTracker = application.getDefaultTracker();
     }
 }
