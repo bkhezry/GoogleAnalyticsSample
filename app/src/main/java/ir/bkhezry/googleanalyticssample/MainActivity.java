d50c8af3c9da780cda442d66e6af103eb844347f
bkhezry <bkhezry@gmail.com>
2016-05-16 12:03:15 +0430

    add some Reciver and permission for non-Google Play devices add tracking code to onResume function.

--- app/src/main/java/ir/bkhezry/googleanalyticssample/MainActivity.java
+++ app/src/main/java/ir/bkhezry/googleanalyticssample/MainActivity.java
@@ -2,13 +2,16 @@ package ir.bkhezry.googleanalyticssample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 
 import ir.bkhezry.googleanalyticssample.Application.AnalyticsApplication;
 
 public class MainActivity extends AppCompatActivity {
     private Tracker mTracker;
+    String name="MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
         // Obtain the shared Tracker instance.
         AnalyticsApplication application = (AnalyticsApplication) getApplication();
         mTracker = application.getDefaultTracker();
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i("bkhezry", "Setting screen name: " + name);
+        mTracker.setScreenName("Image~" + name);
+        mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+
     }
 }
